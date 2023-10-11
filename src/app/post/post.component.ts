0eab0fc408a845c773a7399ff0cbcef39c02fc10
James Ward <jimmywardmichael@gmail.com>
2023-10-11 15:20:45 -0500

    lifecycle hooks continued

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,21 +1,39 @@
-import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output, OnInit, OnChanges, DoCheck, SimpleChanges, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
+export class PostComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
  @Input('img') postImg = ''
  @Output() imgSelected = new EventEmitter<string>()
-
+  
 
 constructor(){
-  console.log('constructor() called')
+  console.log('constructor() called', this.postImg)
 }
 ngOnInit(){
-  console.log('ngOnInit() called');
-
+  console.log('ngOnInit() called' , this.postImg);
+ }
+ ngDoCheck(){
+  console.log('ngDoCheck() called');
+ }
+ ngOnChanges(){
+     console.log('ngOnChanges() called');   
+ }
+ ngAfterViewChecked(){
+    console.log('AfterViewChecked() called');
+    
+ }
+ ngAfterViewInit(){
+    console.log('AfterviewInit() called' );
+ }
+ ngAfterContentInit(){
+  console.log('AfterContentInit() called');
+ }
+ ngAfterContentChecked(){
+  console.log('AfterContentChecked() called');
  }
 }
   
