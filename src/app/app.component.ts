c22c4846f471ff97390c370c38d38b1a77a579db
James Ward <jimmywardmichael@gmail.com>
2023-10-16 13:41:34 -0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-name = 'Luis'
+name = 'luis ramirez'
 imgURL = 'http://picsum.photos/id/237/500/500'
 
 getName() { 
