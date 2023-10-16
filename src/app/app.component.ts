2ee29e2be54deef764dbea8581875967f0253965
James Ward <jimmywardmichael@gmail.com>
2023-10-16 14:42:22 -0500

    done with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
 name = 'luis ramirez'
 imgURL = 'http://picsum.photos/id/237/500/500'
+currentDate = new Date(); 
+cost = 2533000
+tempature = 85.3
+pizza = {
+  toppings: ['pepporoni', 'bacon'],
+  size: 'large'
+}
 
 getName() { 
   return this.name
